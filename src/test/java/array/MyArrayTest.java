b6895c09371577509426c6dfab278613902a3618
Yanping <yanpliu@thoughtworks.com>
2020-04-15 10:53:33 +0800

    [Yanping] init repo

---
+++ src/test/java/array/MyArrayTest.java
@@ -0,0 +1,12 @@
+package array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyArrayTest {
+    @Test
+    public void should_be_true() {
+        assertTrue(true);
+    }
+}
